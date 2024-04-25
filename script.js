e44ef15371cf6b40dbea944b9eec6e6d62fcb3ef
Javier <javie.silvav@duocuc.cl>
2024-04-25 14:28:57 -0400

    Jquery en formulario

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function validarTelefono() {
-    var telefono = document.getElementById('phone');
-    var valorTelefono = telefono.value;
+    var telefono = $('#phone'); 
+    var valorTelefono = telefono.val();
 
     var numeros = /^[0-9]+$/;
 
     if (!numeros.test(valorTelefono)) {
         alert('El número de teléfono solo debe contener dígitos numéricos.');
-
     }
 }
+
+$(document).ready(function() {
+    $('#submitbtn').click(function() {
+        validarTelefono();
+    });
+});
+
